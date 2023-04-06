64cefae0a0e7dccbabb44515fbe88497ff596252
rulanugrh <nextarituenak01@gmail.com>
2023-04-06 12:13:15 +0700

    feat: rabbitmq connection

--- book-services/helpers/rabbitmq.go
+++ book-services/helpers/rabbitmq.go
@@ -1,1 +1,37 @@
 package helpers
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/rulanugrh/alpha/book/config"
+)
+
+var channel *amqp.Channel
+
+func GetConnection() *amqp.Channel {
+	conf := config.GetConfig()
+
+	logon := fmt.Sprintf("amqp://%s:%s@%s:%s",
+		conf.RabbitMQ.User,
+		conf.RabbitMQ.Pass,
+		conf.RabbitMQ.Host,
+		conf.RabbitMQ.Port,
+	)
+
+	conn, err := amqp.Dial(logon)
+	if err != nil {
+		FailOnError(err, "something error for login")
+	}
+	defer conn.Close()
+
+	ch, errs := conn.Channel()
+	if errs != nil {
+		FailOnError(errs, "something error for channel")
+	}
+	defer ch.Close()
+
+	channel = ch
+	return ch
+
+}
