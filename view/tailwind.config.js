a8d47179b0974dfa7e02174698c2aa8afa95a969
rulanugrh <nextarituenak01@gmail.com>
2023-05-28 10:13:43 +0700

    feat: section about

--- view/tailwind.config.js
+++ view/tailwind.config.js
@@ -6,15 +6,20 @@ module.exports = {
       center: true,
       padding: '16px'
     },
-    extend: {},
-    colors: {
-      navy: '#3c486b',
-      merah: '#f45050',
-      kuning: '#f9d949',
-      putih: '#f0f0f0'
-    },
-    fontFamily: {
-      firacode: "'Fira Code', monospace;"
+    extend: {
+      colors: {
+        navy: '#3c486b',
+        merah: '#f45050',
+        kuning: '#f9d949',
+        putih: '#f0f0f0',
+        second: '#ced3e2'
+      },
+      fontFamily: {
+        firacode: "'Fira Code', monospace;"
+      },
+      screens: {
+        '2xl': '1320px'
+      }
     },
   },
   plugins: [],
